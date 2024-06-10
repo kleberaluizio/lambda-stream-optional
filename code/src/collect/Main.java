55d26560f8809ed2085c7a51c787966e80949f60
kleberaluizio <kleberaluizio@gmail.com>
2024-06-10 20:32:42 -0300

    Revendo collect, collectors toMap, groupingBy, partioningBy

--- code/src/collect/Main.java
+++ code/src/collect/Main.java
@@ -28,5 +28,38 @@ public class Main
 		List<Integer> toLinkedList =  list.stream().filter(e -> e % 2 == 0).collect(Collectors.toCollection(
 			LinkedList::new));
 		System.out.println(toLinkedList);
+
+		//Collector - joining - Agrupa em uma única String - Internamente usa String Builder
+		String joining =  list.stream().map(Object::toString).collect(Collectors.joining(";"));
+		System.out.println(joining);
+
+		//Collector - Averaging - Versão para int, double e long
+		Double media =  list.stream().collect(Collectors.averagingInt(n -> n.intValue()));
+		System.out.println(media);
+
+		//Collector - Summing - Versão para int, double e long
+		Integer sum =  list.stream().collect(Collectors.summingInt(n -> n.intValue()));
+		System.out.println(sum);
+
+		//Collector - Summarizing
+		System.out.println("---------------------------");
+		IntSummaryStatistics summary =  list.stream().collect(Collectors.summarizingInt(n -> n.intValue()));
+		System.out.println("summary.getAverage() " + summary.getAverage());
+		System.out.println("summary.getCount() " + summary.getCount());
+		System.out.println("summary.getMax() " + summary.getMax());
+		System.out.println("summary.getMin() " + summary.getMin());
+		System.out.println("summary.getSum() " + summary.getSum());
+
+		//Collector - PartioningBy - sempre retorne uma chave true que atende a condição e false nao atende
+		// Excelente para filtrar dados, exemplo: clientes que atendam uma condição
+		System.out.println("PartioningBy");
+		Map<Boolean, List<Integer>> partioning =  list.stream().collect(Collectors.partitioningBy(n -> n%2 == 0));
+		System.out.println(partioning);
+
+		//Collector - toMap - Recebe duas expressões lambdas, uma para definir como é a chave e outra como é o valor
+		Map<Integer, Integer> toMap =  list.stream().collect(Collectors.toMap(n -> n, n-> n*2));
+		System.out.println(toMap);
+
+
 	}
 }
